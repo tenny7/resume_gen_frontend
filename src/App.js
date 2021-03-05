3053266181afa93bbae14c40480765fd8f433ee5
Ovie Tennyson <atomictenny@gmail.com>
2021-03-05 20:20:19 +0000

    Initial commit for frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+// import {Component}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      apiResponse:"",
+    }
+  }
+
+  callAPI(){
+    fetch("http://localhost:3000/things")
+    .then(res => res.text())
+    .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentWillMount(){
+    this.callAPI();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.apiResponse}</p>
+          
+        </header>
+      </div>
+    );
+  }
+
 }
+
+  
 
 export default App;
